baed090b5da7e11e17507776f1843be843073b51
Maryam Shitu <maryamshitu@gmail.com>
2018-11-25 21:47:52 -0800

    second approch

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -3,5 +3,45 @@
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
 def matrix_convert_to_0(matrix)
-  raise NotImplementedError
+
+
+  zero_column = false
+  zero_column_index = 0
+
+  zero_row_index = []
+
+  matrix.each_with_index do |row, idex|
+      matrix[row].each_with_index do |column, index|
+          if row == 0
+            zero_row_index = 0
+          end
+      end
+  end
+
+  # while row_i < matrix.length
+  #
+  #   column_i = 0
+  #   while column_i < matrix[row_i].length
+  #
+  #     if matrix[row_i][column_i] == 0
+  #       matrix[row_i].each { |num| num = 0 }
+  #
+  #       zero_column = column_i
+  #       zero_column = true
+  #
+  #     end
+  #     column_i += 1
+  #   end
+  #
+  #   if zero_column
+  #     matrix.each do |row|
+  #       row[zero_column_index] = 0
+  #     end
+  #   end
+  #
+  #   row_i += 1
+  # end
+
+
+  return matrix
 end
